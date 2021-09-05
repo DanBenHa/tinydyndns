4b68006b997511ec0cfdc04bebfe19e4ec51a270
Daniel Hähnke <haehnke@gmail.com>
2021-09-05 14:14:12 +0200

    move record updater into separate service

--- ssh/update.sh
+++ ssh/update.sh
@@ -1,12 +1,14 @@
 #!/bin/sh 
 
 TTL=60
+data=/home/dyndns/data
+
 
 get_current_a()
 {
-    domain=$1 
-    data=/etc/tinydns/root/data
-    #linematch=$(grep -noP "(?<=\+$domain.:)[\d\.]*" $data)
+    domain=$1
+    # escape wildcard
+    domain=${domain/\*/"\*"}
     linematch=$(grep -noP "(?<=\+$domain.:)[\d\.]*" $data)
     line=$(echo $linematch | cut -d":" -f1)
     ip4_old=$(echo $linematch | cut -d":" -f2)
@@ -14,35 +16,31 @@ get_current_a()
 
 construct_a()
 {
-    entry=\+$1.:$2:$TTL
+    entry=+$1.:$2:$TTL
     echo $entry
 }
 
 replace_a()
 {
-    data=/etc/tinydns/root/data
     sed -E "$1s/.*/$2/" $data > /tmp/foo
     cat /tmp/foo > $data
 }
 
-make_restart()
-{
-    cd /etc/tinydns/root
-    make
-    pid=$(pgrep tinydns)
-    kill $pid
-    cd /etc/tinydns
-    ./run &
-}
+# make_restart()
+# {
+#     cd /etc/tinydns/root
+#     make
+#     pid=$(pgrep tinydns)
+#     kill $pid
+#     cd /etc/tinydns
+#     ./run &
+# }
 
 
 
-
-fqdn=$1
-ip4_new=$2
+fqdn=$2
+ip4_new=$3
 get_current_a $fqdn
-echo $line
-echo $ip4_old
 construct_a $fqdn $ip4_new
 replace_a $line $entry
-make_restart
+# make_restart
