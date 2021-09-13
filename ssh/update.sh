589339225a2ae0b2280bf15959472e1f043b05eb
Daniel Hähnke <haehnke@gmail.com>
2021-09-13 22:03:29 +0200

    support change of ipv6

--- ssh/update.sh
+++ ssh/update.sh
@@ -3,54 +3,30 @@
 TTL=60
 data=/home/dyndns/data
 
-is_ip()
-{
-    # test for ip4
-    if test -z $(grep -oP "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"); then
-        exit 1
-    else
-        exit 0
-    fi
-}
-
-get_current_a()
+get_current_record()
 {
     domain=$1
+    prefix=$2
+    # escape plus
+    prefix=${prefix/\+/"\+"}
     # escape wildcard
     domain=${domain/\*/"\*"}
-    linematch=$(grep -noP "(?<=\+$domain.:)[\d\.]*" $data)
+    linematch=$(grep -noP "(?<=$prefix$domain.:)[\d\.]*" $data)
     line=$(echo $linematch | cut -d":" -f1)
-    ip4_old=$(echo $linematch | cut -d":" -f2)
+    record_old=$(echo $linematch | cut -d":" -f2)
 }
 
-construct_a()
+construct_record()
 {
-    entry=+$1.:$2:$TTL
+    prefix=$3
+    entry=$3$1.:$2:$TTL
 }
 
 replace_record()
 {
-    sed -E "$1s/.*/$2/" $data > /tmp/foo
-    cat /tmp/foo > $data
-}
-
-construct_txt()
-{
-    entry=\'$1.:$2:$TTL
-}
-
-append_record()
-{
-    echo $1 >> $data
-}
-
-remove_txt()
-{
-    # escape wildcard
-    domain=${fqdn/\*/"\*"}
-    # remove all txt records matching fqdn
-    sed "/^'$domain/d" $data > /tmp/foo
-    cat /tmp/foo > $data
+    sed -E "$1s/.*/$2/" $data > /tmp/data
+    cat /tmp/data > $data
+    rm /tmp/data
 }
 
 
@@ -58,29 +34,66 @@ remove_txt()
 # -c "first second third etc".
 # Therefore, second argument has to be parsed
 if test $# -eq 0; then
-	echo "Illegal number of arguments."
-	exit 1
+    echo "Illegal number of arguments."
+    exit 1
 fi
+
 # split the string
 fqdn=$(echo $2 | cut -d " " -f1)
-ip_new=$(echo $2 | cut -d " " -f2)
-if test $ip_new = "+txt"; then
+arg1=$(echo $2 | cut -d " " -f2)
+
+# Add/remove TXT record
+if test $arg1 = "+txt"; then
     txt=$(echo $2 | cut -d " " -f3) 
-    construct_txt $fqdn $txt
-    append_record $entry
-    exit 0   
+    prefix="\'"
+    construct_record $fqdn $txt $prefix
+    # append TXT
+    echo $entry >> $data
+    exit 0
+elif test $arg1 = "-txt"; then
+    # escape wildcard
+    domain=${fqdn/\*/"\*"}
+    # remove all txt records matching fqdn
+    sed "/^'$domain/d" $data > /tmp/data
+    cat /tmp/data > $data
+    rm /tmp/data
+    exit 0
 fi
-if test $ip_new = "-txt"; then
-    remove_txt $fqdn
-    exit 0   
+
+# If no argument was supplied use IP from SSH_CLIENT
+if test $fqdn = $arg1; then
+    arg1=${SSH_CLIENT%% *}
 fi
-# ip wasn't supplied, use ip from ssh client
-if test $fqdn = $ip_new; then
-	ip_new=${SSH_CLIENT%% *}
+
+# Test if first argument is valid IP
+sipcalc $arg1 > /tmp/sipcalc
+if test $(grep -c "ERR" /tmp/sipcalc) -eq 1
+then
+    echo "Illegal argument."
+    rm /tmp/sipcalc
+    exit 1
 fi
-# check currently set ip and only update if it differs from new ip
-get_current_a $fqdn
-if test $ip_new != $ip4_old; then
-	construct_a $fqdn $ip_new
-	replace_record $line $entry
+
+# real IPs
+if test $(grep -c "ipv4" /tmp/sipcalc) -eq 1
+then
+    ipv=4
+    prefix="+"
+    ip_new=$arg1
+elif test $(grep -c "ipv6" /tmp/sipcalc) -eq 1
+then
+    ipv=6
+    prefix="3"
+    # expand ipv6
+    ip_new=$(grep -e "Expanded Address" /tmp/sipcalc | cut -d " " -f3)
+    # remove the colons
+    ip_new=${ip_new//\:/}
 fi
+# Check currently set IP and only update if it differs from new IP
+get_current_record $fqdn $prefix
+if test $ip_new != $record_old; then
+    construct_record $fqdn $ip_new $prefix
+    replace_record $line $entry
+fi
+rm /tmp/sipcalc
+exit 0
